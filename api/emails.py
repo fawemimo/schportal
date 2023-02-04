b84867ffcdce1263b7abf9cd9d07a96a950f4d2b
fawemimo <f.owolabi81@gmail.com>
2023-02-04 16:33:06 +0100

    short quizze email changes to django email

--- api/emails.py
+++ api/emails.py
@@ -1,4 +1,6 @@
 from templated_mail.mail import BaseEmailMessage
+from django.core.mail import EmailMessage 
+from django.template.loader import render_to_string
 from django.conf import settings
 from datetime import date
 from api.models import Course
@@ -6,7 +8,7 @@ from api.models import Course
 
 def send_inquiries_email(fullname,email,mobile,message):
     try:
-        message = BaseEmailMessage(template_name='api/emails/iniquiries.html',
+        message = BaseEmailMessage(template_name='api/email_response/iniquiries.html',
         context = {
             'fullname' : fullname,
             'email' :email,
@@ -23,7 +25,7 @@ def send_inquiries_email(fullname,email,mobile,message):
 def send_interested_email(course_id,full_name,email,mobile):
     course = Course.objects.get(id=course_id)
     try:
-        message = BaseEmailMessage(template_name='api/emails/interested_emails.html',
+        message = BaseEmailMessage(template_name='api/email_response/interested_emails.html',
         context = {
             'course':course.title.upper(),
             'startdate':course.schedule_set.values('startdate'),
@@ -44,7 +46,7 @@ def send_interested_email(course_id,full_name,email,mobile):
 def send_virtualclass_email(course_id,full_name,email,mobile,remarks):
     course = Course.objects.get(id=course_id)
     try:
-            message = BaseEmailMessage(template_name='api/emails/virtual_class.html',
+            message = BaseEmailMessage(template_name='api/email_response/virtual_class.html',
             context = {
                 'course': course.title,
                 'course_id':course_id,
@@ -62,7 +64,7 @@ def send_virtualclass_email(course_id,full_name,email,mobile,remarks):
 
 def send_kids_coding_email(age_bracket,full_name,email,mobile,remarks):
     try:
-        message = BaseEmailMessage(template_name='api/emails/kidscoding.html',
+        message = BaseEmailMessage(template_name='api/email_response/kidscoding.html',
             context = {
             'age_bracket':age_bracket,
             'full_name':full_name,
@@ -80,8 +82,24 @@ def send_kids_coding_email(age_bracket,full_name,email,mobile,remarks):
 def send_short_quizze_email(fullname,email,mobile,tartiary_education,tartiary_studied,secondary_sch,secondary_studied,tech_interest,more_about_you):
     today_date = date.today()
     try:
-        message = BaseEmailMessage(template_name='api/emails/career_choice.html',
-        context = {
+        # message = BaseEmailMessage(template_name='api/email_response/career_choice.html',
+        # context = {
+        #     'today_date':today_date,
+        #     'fullname' : fullname,
+        #     'email' :email,
+        #     'mobile' : mobile,
+        #     'tartiary_education': tartiary_education,
+        #     'tartiary_studied' : tartiary_studied,
+        #     'secondary_sch' : secondary_sch,
+        #     'secondary_studied' : secondary_studied,
+        #     'tech_interest' : tech_interest,
+        #     'more_about_you': more_about_you, 
+        # }
+        
+        # )
+        # message.send([email,settings.EMAIL_HOST_USER])
+        mail_subject = 'Career Choice Advisory - Anchorsoft Academy '
+        message = render_to_string('api/email_response/career_choice.html', {
             'today_date':today_date,
             'fullname' : fullname,
             'email' :email,
@@ -92,9 +110,10 @@ def send_short_quizze_email(fullname,email,mobile,tartiary_education,tartiary_st
             'secondary_studied' : secondary_studied,
             'tech_interest' : tech_interest,
             'more_about_you': more_about_you, 
-        }
+        })
+        to_email = email, settings.EMAIL_HOST_USER
+        send_email = EmailMessage(mail_subject, message, to=[to_email])
+        send_email.send()
         
-        )
-        message.send([email,settings.EMAIL_HOST_USER])
     except Exception as e:
         print(e)
